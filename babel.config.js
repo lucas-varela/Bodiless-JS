ccd6741b0b1adf76f0ad1b403c1bb46741f25c06
Lucas Varela <lvarelab@its.jnj.com>
2021-11-10 10:47:48 -0300

    Add commonjs transformer to babel on test env

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  "plugins": [
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
-  ]
+  'plugins': [
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
+  ],
+  'env': {
+    'test': {
+      'plugins': [
+        ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+        ['@babel/plugin-proposal-class-properties', { 'loose': true }],
+        ['@babel/plugin-transform-modules-commonjs'],
+      ]
+    }
+  }
 };
